ac237dff9ce034da32dcd93bb5667f37aba54e62
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-01 07:04:48 +0000

    Create solution2 correct for given inputs

--- 2018/day1/solution2.py
+++ 2018/day1/solution2.py
@@ -3,9 +3,22 @@
 
 from pathlib import Path
 
+from .solution1 import parse_input
+
 
 def solve(input_text):
-    return " ".join(input_text) + "?"
+    instructions = parse_input(input_text)
+    frequencies = [0]
+
+    freq = 0
+    while True:
+        for delta in instructions:
+            freq += delta
+
+            if freq in frequencies:
+                return freq
+
+            frequencies.append(freq)
 
 
 if __name__ == '__main__':
