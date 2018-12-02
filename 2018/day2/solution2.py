1e1ae3ab48616d7e372cf62bb56cbb0744283ccf
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-02 07:15:18 +0000

    Solve day2 part2
    There must be a more efficient way to do this!

--- 2018/day2/solution2.py
+++ 2018/day2/solution2.py
@@ -1,11 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from itertools import combinations
 from pathlib import Path
 
 
+def parser(input_text):
+    return [line for line in input_text]
+
+
+def count_differences(text1, text2):
+    # assume lengths agree because they do for known inputs
+        if len(text1) == 0:
+            return 0
+        else:
+            match = 0 if text1[-1] == text2[-1] else 1
+            return match + count_differences(text1[:-1], text2[:-1])
+
+
+def find_off_by_one_pair(box_ids):
+    id_pairs = combinations(box_ids, 2)
+    for id1, id2 in id_pairs:
+        if count_differences(id1, id2) == 1:
+            return(id1, id2)
+
+
+def get_matching_letters(text1, text2):
+    # assume lengths agree because they do for known inputs
+    if len(text1) == 0:
+        return ""
+    match = text1[-1] if text1[-1] == text2[-1] else ""
+    return get_matching_letters(text1[:-1], text2[:-1]) + match
+
+
 def solve(input_text):
-    return " ".join(input_text) + "?"
+    box_ids = parser(input_text)
+    one, two = find_off_by_one_pair(box_ids)
+    return get_matching_letters(one, two)
 
 
 if __name__ == '__main__':
--- 2018/day2/tests.py
+++ 2018/day2/tests.py
@@ -59,6 +59,38 @@ class TestSolution2(TestSolution):
     module = solution2
     input_filename = "test_input2.txt"
     expected = "fgij"
+    box_ids = [
+        "abcde",
+        "fghij",
+        "klmno",
+        "pqrst",
+        "fguij",
+        "axcye",
+        "wvxyz",
+    ]
+
+    def test_parser(self):
+        self.assertEqual(self.box_ids, solution2.parser(self.input_text))
+
+    def test_count_differences(self):
+        self.assertEqual(5, solution2.count_differences(self.box_ids[0],
+                                                        self.box_ids[1]))
+        self.assertEqual(2, solution2.count_differences(self.box_ids[0],
+                                                        self.box_ids[5]))
+        self.assertEqual(1, solution2.count_differences(self.box_ids[1],
+                                                        self.box_ids[4]))
+
+    def test_find_off_by_one_pair(self):
+        self.assertEqual(("fghij", "fguij"),
+                         solution2.find_off_by_one_pair(self.box_ids))
+
+    def test_get_matching_letters(self):
+        self.assertEqual("", solution2.get_matching_letters(
+            self.box_ids[0], self.box_ids[1]))
+        self.assertEqual("ace", solution2.get_matching_letters(
+            self.box_ids[0], self.box_ids[5]))
+        self.assertEqual("fgij", solution2.get_matching_letters(
+            self.box_ids[1], self.box_ids[4]))
 
     def test_solver(self):
         solution = self.module.solve(self.input_text)
