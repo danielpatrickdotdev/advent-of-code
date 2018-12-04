fa3bd1d02909b517ff78c65521b59441a450deb7
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-04 06:51:03 +0000

    Create a basic parser for day4 part1

--- 2018/day4/solution1.py
+++ 2018/day4/solution1.py
@@ -1,8 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from collections import defaultdict
+from datetime import date, timedelta
 from pathlib import Path
+import re
 
+
+shift_event_regex = re.compile("^\[1518-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] ("
+                               "(?:Guard #\d+ begins shift)|"
+                               "(?:falls asleep)|"
+                               "(?:wakes up)"
+                               ")$")
+
+def parse(input_text):
+    shifts = defaultdict(dict)
+    for event_string in input_text:
+        match = shift_event_regex.match(event_string)
+        month, day, hour, minute, event = match.groups()
+
+        month = int(month)
+        day = int(day)
+        hour = int(hour)
+        minute = int(minute)
+
+        if hour == 23:
+            shift_date = date(1518, month, day) + timedelta(days=1)
+            month = shift_date.month
+            day = shift_date.day
+            minute = minute - 60
+
+        shifts[(month, day)][minute] = event
+
+    return shifts
 
 def solve(input_text):
     return " ".join(input_text) + "!"
--- 2018/day4/tests.py
+++ 2018/day4/tests.py
@@ -32,6 +32,41 @@ class TestSolution(unittest.TestCase):
 class TestSolution1(TestSolution):
     module = solution1
     expected = "lorem ipsum!"
+    input_as_list = [
+        "[1518-11-01 00:00] Guard #10 begins shift",
+        "[1518-11-01 00:05] falls asleep",
+        "[1518-11-01 00:25] wakes up",
+        "[1518-11-01 00:30] falls asleep",
+        "[1518-11-01 00:55] wakes up",
+        "[1518-11-01 23:58] Guard #99 begins shift",
+        "[1518-11-02 00:40] falls asleep",
+        "[1518-11-02 00:50] wakes up",
+        "[1518-11-03 00:05] Guard #10 begins shift",
+        "[1518-11-03 00:24] falls asleep",
+        "[1518-11-03 00:29] wakes up",
+        "[1518-11-04 00:02] Guard #99 begins shift",
+        "[1518-11-04 00:36] falls asleep",
+        "[1518-11-04 00:46] wakes up",
+        "[1518-11-05 00:03] Guard #99 begins shift",
+        "[1518-11-05 00:45] falls asleep",
+        "[1518-11-05 00:55] wakes up",
+    ]
+
+    def test_parser(self):
+        shifts = self.module.parse(self.input_as_list)
+        self.assertEqual(5, len(shifts))
+
+        shift1 = shifts[(11, 1)]
+        self.assertEqual("Guard #10 begins shift", shift1[0])
+        self.assertEqual("falls asleep", shift1[5])
+        self.assertEqual("wakes up", shift1[25])
+        self.assertEqual("falls asleep", shift1[30])
+        self.assertEqual("wakes up", shift1[55])
+
+        shift2 = shifts[(11, 2)]
+        self.assertEqual("Guard #99 begins shift", shift2[-2])
+        self.assertEqual("falls asleep", shift2[40])
+        self.assertEqual("wakes up", shift2[50])
 
     def test_solver(self):
         solution = self.module.solve(self.input_text)
