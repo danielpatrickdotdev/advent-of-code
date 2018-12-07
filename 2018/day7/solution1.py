0c28e9e5c66e6963a0a51a7356e4d58f92191bcc
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-07 06:37:57 +0000

    Solve day7 part1

--- 2018/day7/solution1.py
+++ 2018/day7/solution1.py
@@ -3,9 +3,24 @@
 
 from pathlib import Path
 
+from .common import get_available, get_steps, parse
+
 
 def solve(input_text):
-    return " ".join(input_text) + "!"
+    requirements = parse(input_text)
+    steps = get_steps(requirements)
+
+    result = []
+
+    while len(steps) > 0:
+        available = get_available(requirements, steps)
+        next_char = sorted(list(available))[0]
+        steps.remove(next_char)
+        if next_char in requirements:
+            del requirements[next_char]
+        result.append(next_char)
+
+    return "".join(result)
 
 
 if __name__ == '__main__':
