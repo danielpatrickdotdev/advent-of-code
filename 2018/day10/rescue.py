8e6d888084bd7f598c81d756eb51c3062bb89da2
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-10 11:47:50 +0000

    Refactor setting of boundaries in RescueMessage

--- 2018/day10/rescue.py
+++ 2018/day10/rescue.py
@@ -8,22 +8,34 @@ from collections import defaultdict
 class RescueMessage:
     def __init__(self, data):
         self.grid = self.construct_grid()
+        self.set_boundaries(data)
+
+        for (x, y), (dx, dy) in data:
+            self.grid[y][x].append((dy, dx))
+
+    def set_boundaries(self, data):
+        self.reset_boundaries()
+        for (x, y), (dx, dy) in data:
+            self.update_x_boundaries(x)
+            self.update_y_boundaries(y)
+
+    def reset_boundaries(self):
         self.min_col = None
         self.max_col = None
         self.min_row = None
         self.max_row = None
 
-        for (x, y), (dx, dy) in data:
-            if self.min_col is None or x < self.min_col:
-                self.min_col = x
-            if self.max_col is None or x > self.max_col:
-                self.max_col = x
-            if self.min_row is None or y < self.min_row:
-                self.min_row = y
-            if self.max_row is None or y > self.max_row:
-                self.max_row = y
+    def update_x_boundaries(self, x):
+        if self.min_col is None or x < self.min_col:
+            self.min_col = x
+        if self.max_col is None or x > self.max_col:
+            self.max_col = x
 
-            self.grid[y][x].append((dy, dx))
+    def update_y_boundaries(self, y):
+        if self.min_row is None or y < self.min_row:
+            self.min_row = y
+        if self.max_row is None or y > self.max_row:
+            self.max_row = y
 
     def construct_grid(self):
         return defaultdict(lambda: defaultdict(list))
