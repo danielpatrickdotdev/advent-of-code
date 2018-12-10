03927ad9773bfc45dab3e3fe302d3985dc1577f9
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-10 07:58:14 +0000

    Solve day10 part2

--- 2018/day10/solution2.py
+++ 2018/day10/solution2.py
@@ -4,8 +4,15 @@
 from pathlib import Path
 
 
+from .common import parse, find_time_with_closest_fit
+
+
 def solve(input_text):
-    return " ".join(input_text) + "?"
+    data = parse(input_text)
+
+    best_time = find_time_with_closest_fit(data[:100])
+
+    return best_time
 
 
 if __name__ == '__main__':
