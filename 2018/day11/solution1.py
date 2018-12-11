9e5440356d89c090317321f2a0b5fdac240337cd
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-11 07:48:00 +0000

    Simplify tests, add some test values (failing)

--- 2018/day11/solution1.py
+++ 2018/day11/solution1.py
@@ -1,22 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 
-
-def solve(input_text):
-    return " ".join(input_text) + "!"
+def solve(input_value):
+    return "{}!".format(input_value)
 
 
 if __name__ == '__main__':
-    from shared.utils import get_input
     from timeit import default_timer as timer
 
     start = timer()
 
-    input_path = Path(__file__).parent.joinpath("input.txt")
-    input_text = get_input(input_path)
-    solution = solve(input_text)
+    input_value = 9798
+    solution = solve(input_value)
     print(solution)
 
     end = timer()
