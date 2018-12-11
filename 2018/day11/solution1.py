b7a8ead60b51370cf037737248fbb72fbfe91b0d
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-11 08:21:23 +0000

    Create grid

--- 2018/day11/solution1.py
+++ 2018/day11/solution1.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 
 
+def get_cell_power_level(x, y, serial):
+    rack_id = x + 10
+    return (((rack_id * y + serial) * rack_id) % 1000) // 100 - 5
+
+
+def create_grid(serial):
+    return [
+        [get_cell_power_level(x, y, serial) for y in range(1, 301)]
+        for x in range(1, 301)
+    ]
+
+
 def solve(input_value):
     return "{}!".format(input_value)
 
--- 2018/day11/tests.py
+++ 2018/day11/tests.py
@@ -9,6 +9,19 @@ from . import solution1, solution2
 class TestSolution1(unittest.TestCase):
     module = solution1
 
+    def test_get_cell_power_level(self):
+        self.assertEqual(4, self.module.get_cell_power_level(3, 5, 8))
+        self.assertEqual(-5, self.module.get_cell_power_level(122, 79, 57))
+        self.assertEqual(0, self.module.get_cell_power_level(217, 196, 39))
+        self.assertEqual(4, self.module.get_cell_power_level(101, 153, 71))
+
+    def test_create_grid(self):
+        grid = self.module.create_grid(18)
+        self.assertEqual(300, len(grid))
+        self.assertEqual(300, len(grid[0]))
+        self.assertEqual([-2, -4, 4, 1, -1], grid[31][43:48])
+        self.assertEqual([4, -5, -4, -3, -2], grid[35][43:48])
+
     def test_solver(self):
         self.assertEqual(29, self.module.solve(18))
         self.assertEqual(30, self.module.solve(42))
