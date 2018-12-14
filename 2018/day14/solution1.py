e709253c7593d8218805e404320bbbc4a2fe31ff
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-14 04:55:13 +0000

    Create day14 from template

---
+++ 2018/day14/solution1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+
+
+def solve(input_text):
+    return " ".join(input_text) + "!"
+
+
+if __name__ == '__main__':
+    from shared.utils import get_input
+    from timeit import default_timer as timer
+
+    start = timer()
+
+    input_path = Path(__file__).parent.joinpath("input.txt")
+    input_text = get_input(input_path)
+    solution = solve(input_text)
+    print(solution)
+
+    end = timer()
+    print()
+    print("-" * 80)
+    print("Time elapsed: {:.3f}s".format(end - start))
