3162a5fc4c9ed26af1c18711f06fcd24697852e3
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-14 05:19:34 +0000

    Remove unneeded files, update test/real inputs

--- 2018/day14/solution1.py
+++ 2018/day14/solution1.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 
-
-def solve(input_text):
-    return " ".join(input_text) + "!"
+def solve(input_value):
+    return "{}".format(input_value)
 
 
 if __name__ == '__main__':
-    from shared.utils import get_input
     from timeit import default_timer as timer
 
     start = timer()
 
-    input_path = Path(__file__).parent.joinpath("input.txt")
-    input_text = get_input(input_path)
-    solution = solve(input_text)
+    solution = solve(540391)
     print(solution)
 
     end = timer()
--- 2018/day14/tests.py
+++ 2018/day14/tests.py
@@ -1,50 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 import unittest
 
-from shared.utils import get_input
 from . import solution1, solution2
 
 
-SOLUTION_DIR = Path(__file__).parent
-
-
-class TestSolution(unittest.TestCase):
-    module = None
-    input_filename = "test_input.txt"
-    expected = None
-
-    def setUp(self):
-        if self.module is None:
-            raise NotImplementedError(
-                "subclasses of TestSolution must provide module to test"
-            )
-        if self.expected is None:
-            raise NotImplementedError(
-                "subclasses of TestSolution must provide expected value"
-            )
-        self.input_path = SOLUTION_DIR.joinpath(self.input_filename)
-        self.input_text = get_input(self.input_path)
-
-
-class TestSolution1(TestSolution):
+class TestSolution1(unittest.TestCase):
     module = solution1
-    expected = "lorem ipsum!"
 
     def test_solver(self):
-        solution = self.module.solve(self.input_text)
-        self.assertEqual(self.expected, solution)
+        self.assertEqual("5158916779", self.module.solve(9))
+        self.assertEqual("0124515891", self.module.solve(5))
+        self.assertEqual("9251071085", self.module.solve(18))
+        self.assertEqual("5941429882", self.module.solve(2018))
 
 
-class TestSolution2(TestSolution):
+class TestSolution2(unittest.TestCase):
     module = solution2
     expected = "lorem ipsum?"
 
     def test_solver(self):
-        solution = self.module.solve(self.input_text)
-        self.assertEqual(self.expected, solution)
+        self.assertEqual(self.expected, self.module.solve("lorem ipsum"))
 
 
 if __name__ == '__main__':
