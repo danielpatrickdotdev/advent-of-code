629c4d16bdb3976025aeafeee0179a90f4788ff9
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-21 12:24:30 +0000

    Reverse engineer input for part 1

--- 2018/day21/solution1.py
+++ 2018/day21/solution1.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from pathlib import Path
 
-from .program import Program
+def solve():
+    one = two = three = five = 0
 
+    five = 65_536
+    two = 2_238_642
 
-def solve(input_text):
-    program = Program(input_text)
-    program.execute(28)
-    return program.register[2]
+    end = False
+    while not end:
+        three = five & 255
+        two += three
+        two &= 16_777_215
+        two *= 65_899
+        two &= 16_777_215
+
+        if 256 > five:
+            break
+
+        three = 0
+
+        while five >= 256:
+            one = three + 1
+            one *= 256
+            if one > five:
+                five = three
+                break
+            three += 1
+
+    return two
 
 
 if __name__ == '__main__':
-    from shared.utils import get_input
     from timeit import default_timer as timer
 
     start = timer()
 
-    input_path = Path(__file__).parent.joinpath("input.txt")
-    input_text = get_input(input_path)
-    solution = solve(input_text)
+    solution = solve()
     print(solution)
 
     end = timer()
