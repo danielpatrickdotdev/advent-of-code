d6ec801cf1514c2d0df61061c819f2c0dbbf3a69
Daniel Patrick <danielpatrick@hotmail.com>
2018-12-24 19:44:04 +0000

    Create Caves class

---
+++ 2018/day22/caves.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class Caves:
+    cave_risks = {
+        "M": 0,
+        "T": 0,
+        ".": 0,
+        "=": 1,
+        "|": 2,
+    }
+
+    def __init__(self, depth, x, y):
+        self.depth = depth
+        self.target = (x, y)
+        self.create_caverns(x + 1, y + 1)
+
+    def get_geology(self, x, y):
+        return self.geology[y][x]
+
+    def get_erosion(self, x, y):
+        return self.erosion[y][x]
+
+    def get_type(self, x, y):
+        return self.caves[y][x]
+
+    def get_risk(self, x, y):
+        return self.cave_risks[self.get_type(x, y)]
+
+    def create_caverns(self, width, height):
+        def set_geology(x, y):
+            if (x, y) in [(0, 0), self.target]:
+                self.geology[-1].append(0)
+            elif y == 0:
+                self.geology[-1].append(x * 16_807)
+            elif x == 0:
+                self.geology[-1].append(y * 48_271)
+            else:
+                self.geology[-1].append(
+                    self.get_erosion(x, y - 1) * self.get_erosion(x - 1, y)
+                )
+
+        def set_erosion(x, y):
+            self.erosion[-1].append(
+                (self.get_geology(x, y) + self.depth) % 20_183
+            )
+
+        def set_cave_type(x, y):
+            cave_types = ".=|"
+
+            if x == y == 0:
+                cave_type = "M"  # mouth
+            elif (x, y) == self.target:
+                cave_type = "T"  # target
+            else:
+                cave_type = cave_types[self.get_erosion(x, y) % 3]
+
+            self.caves[-1].append(cave_type)
+
+        self.geology = []
+        self.erosion = []
+        self.caves = []
+
+        for y in range(height):
+            self.geology.append([])
+            self.erosion.append([])
+            self.caves.append([])
+
+            for x in range(width):
+                set_geology(x, y)
+                set_erosion(x, y)
+                set_cave_type(x, y)
+
+    def __str__(self):
+        return "\n".join(
+            "".join(row) for row in self.caves
+        )
--- 2018/day22/tests.py
+++ 2018/day22/tests.py
@@ -6,6 +6,7 @@ import unittest
 
 from shared.utils import get_input
 from . import solution1, solution2, common
+from .caves import Caves
 
 
 SOLUTION_DIR = Path(__file__).parent
@@ -39,6 +40,55 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(24680, depth)
         self.assertEqual(12345, target_x)
         self.assertEqual(67890, target_y)
+
+
+class TestCaves(unittest.TestCase):
+    caves_string = (
+        "M=.|=.|.|=.\n"
+        ".|=|=|||..|\n"
+        ".==|....||=\n"
+        "=.|....|.==\n"
+        "=|..==...=.\n"
+        "=||.=.=||=|\n"
+        "|.=.===|||.\n"
+        "|..==||=.|=\n"
+        ".=..===..=|\n"
+        ".======|||=\n"
+        ".===|=|===T"
+    )
+
+    def test_constructor(self):
+        caves = Caves(510, 10, 10)
+        self.assertEqual(0, caves.get_geology(0, 0))
+        self.assertEqual(16_807, caves.get_geology(1, 0))
+        self.assertEqual(48_271, caves.get_geology(0, 1))
+        self.assertEqual(145_722_555, caves.get_geology(1, 1))
+        self.assertEqual(0, caves.get_geology(10, 10))
+        self.assertEqual(self.caves_string, str(caves))
+
+    def test_get_geology(self):
+        caves = Caves(510, 10, 10)
+        self.assertEqual(145_722_555, caves.get_geology(1, 1))
+
+    def test_get_erosion(self):
+        caves = Caves(510, 10, 10)
+        self.assertEqual(1805, caves.get_erosion(1, 1))
+
+    def test_get_type(self):
+        caves = Caves(510, 10, 10)
+        self.assertEqual("M", caves.get_type(0, 0))
+        self.assertEqual("=", caves.get_type(1, 0))
+        self.assertEqual(".", caves.get_type(0, 1))
+        self.assertEqual("|", caves.get_type(1, 1))
+        self.assertEqual("T", caves.get_type(10, 10))
+
+    def test_get_risk(self):
+        caves = Caves(510, 10, 10)
+        self.assertEqual(0, caves.get_risk(0, 0))
+        self.assertEqual(1, caves.get_risk(1, 0))
+        self.assertEqual(0, caves.get_risk(0, 1))
+        self.assertEqual(2, caves.get_risk(1, 1))
+        self.assertEqual(0, caves.get_risk(10, 10))
 
 
 class TestSolution1(TestSolution):
