86f3fec690497c8b1f350aea0593c90f1cec5d82
Daniel Patrick <danielpatrick@hotmail.com>
2019-02-06 10:20:04 +0000

    Solve 2015 day02

---
+++ 2015/day02/src/main.rs
@@ -0,0 +1,45 @@
+use std::fs;
+mod present;
+use self::present::Present;
+
+fn solve_part1(puzzle_input: &str) -> i32 {
+    puzzle_input.lines()
+        .map(|line| Present::new(line))
+        .map(|present| present.wrapping_paper_required())
+        .sum()
+}
+
+fn solve_part2(puzzle_input: &str) -> i32 {
+    puzzle_input.lines()
+        .map(|line| Present::new(line))
+        .map(|present| present.ribbon_required())
+        .sum()
+}
+
+fn main() {
+    let puzzle_input = fs::read_to_string("input/input.txt")
+        .expect("problem reading file");
+
+    println!("Part1: {}", solve_part1(&puzzle_input));
+
+    println!("Part2: {}", solve_part2(&puzzle_input));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let test_input = "2x3x4\n1x1x10";
+
+        assert_eq!(101, solve_part1(&test_input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let test_input = "2x3x4\n1x1x10";
+
+        assert_eq!(48, solve_part2(&test_input));
+    }
+}
---
+++ 2015/day02/src/present.rs
@@ -0,0 +1,124 @@
+use std::str::Split;
+
+#[derive(Debug)]
+pub struct Present {
+    l: i32,
+    w: i32,
+    h: i32,
+}
+
+impl Present {
+    pub fn new(s: &str) -> Self {
+        let mut iter = s.split("x");
+        let l = Present::parse_dim(&mut iter, "length");
+        let w = Present::parse_dim(&mut iter, "width");
+        let h = Present::parse_dim(&mut iter, "height");
+
+        Present {l, w, h}
+    }
+
+    pub fn wrapping_paper_required(&self) -> i32 {
+        self.surface_area() + self.smallest_side_area()
+    }
+
+    pub fn ribbon_required(&self) -> i32 {
+        let mut dims = vec![self.l, self.w, self.h];
+        dims.sort_unstable_by(|a, b| b.cmp(a));
+
+        let wrap = 2 * dims.pop().unwrap() + 2 * dims.pop().unwrap();
+
+        wrap + self.l * self.w * self.h
+    }
+
+    // Private helper functions & methods
+
+    fn parse_dim(iter: &mut Split<&str>, dim: &str) -> i32 {
+        iter.next().expect(&format!("No {} provided", dim))
+            .parse().expect(&format!("Invalid {} provided", dim))
+    }
+
+    fn surface_area(&self) -> i32 {
+        2 * self.l * self.w + 2 * self.w * self.h + 2 * self.h * self.l
+    }
+
+    fn smallest_side_area(&self) -> i32 {
+        let mut dims = vec![self.l, self.w, self.h];
+        dims.sort_unstable_by(|a, b| b.cmp(a));
+        dims.pop().unwrap() * dims.pop().unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_constructor() {
+        let to_test: [(&str, i32, i32, i32); 2] = [
+            ("1x1x1", 1, 1, 1),
+            ("11x22x33", 11, 22, 33),
+        ];
+
+        for (input, l, w, h) in to_test.iter() {
+            let p = Present::new(&input);
+            assert_eq!(l, &p.l);
+            assert_eq!(w, &p.w);
+            assert_eq!(h, &p.h);
+        }
+    }
+
+    #[test]
+    fn test_surface_area() {
+        let to_test = [
+            (2, 3, 4, 52),
+            (1, 1, 10, 42),
+        ];
+
+        for (l, w, h, area) in to_test.iter() {
+            let p = Present {l: *l, w: *w, h: *h};
+            assert_eq!(*area, p.surface_area());
+        }
+    }
+
+    #[test]
+    fn test_smallest_side_area() {
+        let to_test = [
+            (2, 3, 4, 6),
+            (1, 1, 10, 1),
+            (10, 1, 1, 1),
+        ];
+
+        for (l, w, h, area) in to_test.iter() {
+            let p = Present {l: *l, w: *w, h: *h};
+            assert_eq!(*area, p.smallest_side_area());
+        }
+    }
+
+    #[test]
+    fn test_wrapping_paper_required() {
+        let to_test = [
+            (2, 3, 4, 58),
+            (1, 1, 10, 43),
+            (10, 1, 1, 43),
+        ];
+
+        for (l, w, h, area) in to_test.iter() {
+            let p = Present {l: *l, w: *w, h: *h};
+            assert_eq!(*area, p.wrapping_paper_required());
+        }
+    }
+
+    #[test]
+    fn test_ribbon_required() {
+        let to_test = [
+            (2, 3, 4, 34),
+            (1, 1, 10, 14),
+            (10, 1, 1, 14),
+        ];
+
+        for (l, w, h, length) in to_test.iter() {
+            let p = Present {l: *l, w: *w, h: *h};
+            assert_eq!(*length, p.ribbon_required());
+        }
+    }
+}
